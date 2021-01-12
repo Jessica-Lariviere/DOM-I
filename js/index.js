e96ad876278394ec8d3fdc66ee9cdf4d806e8fdf
Jessixdanielle <45211337+Jessixdanielle@users.noreply.github.com>
2021-01-11 20:39:28 -0500

    finished updating the code to take from the object rather than directly change it piece by piece with selectors

--- js/index.js
+++ js/index.js
@@ -46,12 +46,12 @@ logo.setAttribute('src', siteContent["nav"]["img-src"])
 
 let nav = document.querySelectorAll("nav a");
 
-nav[0].textContent= "Services";
-nav[1].textContent= "Product";
-nav[2].textContent= "Vision";
-nav[3].textContent= "Features";
-nav[4].textContent= "About";
-nav[5].textContent= "Contact";
+nav[0].textContent= siteContent["nav"]["nav-item-1"];
+nav[1].textContent= siteContent["nav"]["nav-item-2"];
+nav[2].textContent= siteContent["nav"]["nav-item-3"];
+nav[3].textContent= siteContent["nav"]["nav-item-4"];
+nav[4].textContent= siteContent["nav"]["nav-item-5"];
+nav[5].textContent= siteContent["nav"]["nav-item-6"];
 
 
 // h1, button, img
@@ -61,42 +61,69 @@ let mainButton = document.querySelector(".cta-text button");
 
 let hero = document.querySelector("#cta-img")
 
-h1.textContent = "DOM IS AWESOME!"
+h1.textContent = siteContent["cta"]["h1"]
 
-mainButton.textContent = "Get Started"
+mainButton.textContent = siteContent["cta"]["button"]
 
-hero.src = 'img/header-img.png'
+hero.src = siteContent["cta"]["img-src"]
 //
 
 //main
 
 let infoHeader = document.querySelectorAll(".text-content h4")
 
-infoHeader[0].textContent = "Features";
-infoHeader[1].textContent = "About";
-infoHeader[2].textContent = "Services";
-infoHeader[3].textContent = "Product";
-infoHeader[4].textContent = "Vision";
+infoHeader[0].textContent = siteContent["main-content"]["features-h4"];
+
+infoHeader[1].textContent = siteContent["main-content"]["about-h4"];
+
+infoHeader[2].textContent = siteContent["main-content"]["services-h4"];
+
+infoHeader[3].textContent = siteContent["main-content"]["product-h4"];
+
+infoHeader[4].textContent = siteContent["main-content"]["vision-h4"];
 
 let infoP= document.querySelectorAll(".text-content p")
 
-infoP[0].textContent = "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+infoP[0].textContent = siteContent["main-content"]["features-content"];
 
-infoP[1].textContent = "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+infoP[1].textContent = siteContent["main-content"]["about-content"];
 
-infoP[2].textContent = "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+infoP[2].textContent = siteContent["main-content"]["services-content"];
 
-infoP[3].textContent = "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+infoP[3].textContent = siteContent["main-content"]["product-content"];
 
-infoP[4].textContent = "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+infoP[4].textContent = siteContent["main-content"]["vision-content"];
 
 
 let midPage = document.querySelector("#middle-img");
 
-midPage.src = "img/mid-page-accent.jpg";
+midPage.src = siteContent["main-content"]["middle-img-src"];
 
 //Contact
+// "contact": {
+//  "contact-h4" : "Contact",
+//  "address" : "123 Way 456 Street Somewhere, USA",
+//  "phone" : "1 (888) 888-8888",
+//  "email" : "sales@greatidea.io",
+//},
+
+let contactTitle = document.querySelector(".contact h4");
+
+let contact = document.querySelectorAll(".contact p");
+
+contactTitle.textContent = siteContent["contact"]["contact-h4"];
+
+contact[0].textContent = siteContent["contact"]["address"];
+
+contact[1].textContent = siteContent["contact"]["phone"];
+
+contact[2].textContent= siteContent["contact"]["email"];
+
 
 
 
 //Footer
+
+let copyright = document.querySelector("footer p");
+
+copyright.textContent = siteContent["footer"]["copyright"];
